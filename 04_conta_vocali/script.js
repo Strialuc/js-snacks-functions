58cdda43e0a2bcc779393e3848079a26a266b89d
Luca Striano <luca.striano90@gmail.com>
2025-12-09 18:18:20 +0100

    snack 4 "conta vocali" terminato

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -2,12 +2,43 @@
 
 const word = 'javascript';
 
+const vocali = ["a","e","i","o","u"]
 
 // Dichiara la funzione qui.
+
+function controlloVocali (array1, array2) {
+
+    let count = 0;
+
+    const result= [];
+
+    for (i=0; i<array1.length; i++) {
+
+       const lettera = array1[i];
+
+       if (array2.includes(lettera)) {
+            result.push = lettera;
+            console.log(result);
+            count++;
+        }
+        
+
+    }
+
+
+    return count;
+
+    return result;
+
+}
+
+    
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const risultato = controlloVocali(word, vocali);
+console.log(risultato);
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
