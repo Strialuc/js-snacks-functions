eb8309c172e1a83fc893c9d1a81e65a5fc8f7d71
Luca Striano <luca.striano90@gmail.com>
2025-12-09 17:08:45 +0100

    snack 3 " filtra iniziali completato"

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -2,12 +2,35 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
+let output = [];
 
 // Dichiara la funzione qui.
+
+function snack (cont1, cont2) {
+
+    const result=[];
+
+    for (let i = 0 ; i < cont1.length ; i++ ) {
+
+        if ((cont1[i].charAt(0)) === cont2) {
+
+            result.push(cont1[i]);
+        }
+
+        
+    }
+
+    return result;
+}
+
+
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+output = snack(names, "A");
+
+console.log(output);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
