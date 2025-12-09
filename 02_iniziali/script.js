d3fbe93fc7ccfa16b7054946f59d34dd47ea8b73
Luca Striano <luca.striano90@gmail.com>
2025-12-09 16:35:45 +0100

    secondo  snack "iniziali completato"

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -2,12 +2,29 @@
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
+let arrayOutput = [];
+
 
 // Dichiara la funzione qui.
+
+function primeLettere (cont) {
+
+    const result= [];
+
+    for (let i= 0 ; i < cont.length ; i++ ) {
+
+       result.push(cont[i].charAt(0));
+    }
+
+    return result;
+}
 
 
 // Invoca la funzione qui e stampa il risultato in console
 
+arrayOutput = primeLettere(names)
+
+console.log(arrayOutput);
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
